5c4e0bf90da2e6adfb27fbda53ffa1692f8d31fe
Serch & Soto <111457097+Serchisoto@users.noreply.github.com>
2026-07-23 14:53:44 -0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,40 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.1.75'],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/destinations/cancun',
+        permanent: false,
+      },
+      {
+        source: '/cancun',
+        destination: '/destinations/cancun',
+        permanent: true,
+      },
+      {
+        source: '/admin',
+        destination: '/admin/dashboard',
+        permanent: true,
+      },
+      {
+        source: '/provider',
+        destination: '/provider/dashboard',
+        permanent: true,
+      },
+    ]
+  },
+  images: {
+    remotePatterns: [
+{
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
